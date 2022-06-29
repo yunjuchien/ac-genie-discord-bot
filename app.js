429212b48aa603a2f8dbc0e8c6ecab8f619e7bdc
Victor Neo <icyisamu@gmail.com>
2022-06-28 23:53:48 -0500

    Time limited special scoldings

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token, naughtyStudents } = require('./config.json');
+const { token, naughtyStudents, specialStudents } = require('./config.json');
 
 // 全部的句的檔案在 quotes.json 裡
 // naughtyStudents 是特別調皮的同學們的 Discord IDs
-const { scoldings, motivation, extra } = require('./quotes.json');
+const { scoldings, motivation, extra, special } = require('./quotes.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -33,6 +33,9 @@ client.on('interactionCreate', async interaction => {
     if (naughtyStudents.includes(interaction.user.id)) {
       // 給特別的同學們特別的「關照」
       await interaction.reply(getRandomItem(extra));
+    }
+    else if (specialStudents.includes(interaction.user.id)) {
+      await interaction.reply(getRandomItem(special));
     }
     else {
     // 給其他的同學們一般的「關照」
